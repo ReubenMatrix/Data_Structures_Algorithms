86e0e908fe385aeba5590fb39e08d88beb681eee
Reubemx <110336575+Reubemx@users.noreply.github.com>
2026-06-11 23:27:22 +0530

    greedy

--- arrays/greedy/gasStation.cpp
+++ arrays/greedy/gasStation.cpp
@@ -2,24 +2,29 @@
 #include <vector>
 using namespace std;
 
-int solution(vector<int>& gas, vector<int>& cost) {
-    int totalGas = 0;
-    int tank = 0;
-    int start = 0;
-
-    for (int i = 0; i < gas.size(); i++) {
-        int diff = gas[i] - cost[i];
-
-        totalGas += diff;
-        tank += diff;
-
-        if (tank < 0) {
-            start = i + 1;
-            tank = 0;
+int solution(vector<int> gas, vector<int> cost) {
+  int totalGas = 0, totalCost = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
         }
-    }
 
-    return totalGas >= 0 ? start : -1;
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        int currentGas = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            currentGas += gas[i] - cost[i];
+            if (currentGas < 0) {
+                currentGas = 0;
+                start = i + 1;
+            }
+        }
+
+        return start; 
 }
 
 int main() {
